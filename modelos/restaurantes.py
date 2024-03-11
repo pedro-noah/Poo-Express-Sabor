875503cad6d11ba33fd2d996916451a953be2142
Noah <pedromilarski11@gmail.com>
2024-03-11 17:25:08 -0300

    Atualização de Restaurante
    Adicionei Boas Práticas

--- modelos/restaurantes.py
+++ modelos/restaurantes.py
@@ -1,14 +1,34 @@
 class Restaurante:
-    nome = ''
-    categoria = ''
-    ativo = False
+    restaurantes = []
+    def __init__(self, nome, categoria): 
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
+        self._ativo = False
+        Restaurante.restaurantes.append(self)
+        
+    def __str__(self):
+        return f'{self._nome} | {self._categoria}'
+    
+    @classmethod
+    def listar_restaurantes(cls):
+        print(f'{'NOME'.ljust(25)} | {'CATEGORIA'.ljust(25)} | {'STATUS'}')
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
+            
+    @property
+    def ativo(self):
+        return '✓' if self._ativo else '✕'    
+    
+    def alternar_estado(self):
+        self._ativo = not self._ativo
 
-restaurante_praca = Restaurante()
-restaurante_praca.nome = 'Praça'
-restaurante_praca.categoria = 'Gourmet'
+restaurante_praca = Restaurante('Praça', 'Francesa')
+restaurante_praca.alternar_estado()
+restaurante_pizza = Restaurante('PizzaEX', 'Italiana')
 
-restaurante_pizza = Restaurante()
+Restaurante.listar_restaurantes()
 
-restaurantes = [restaurante_praca, restaurante_pizza]
+@property
+def ativo(self):
+    return '✓' if self.ativo else '✕'
 
-print(restaurante_praca.ativo)
